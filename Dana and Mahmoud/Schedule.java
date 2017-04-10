ce03d44e27d6785b19d6ff778463b0989f9c64ae
Dana <dsa28@mail.aub.edu>
2017-04-10 09:04:16 +0300

    Classes to implement schedules

---
+++ Dana and Mahmoud/Schedule.java
@@ -0,0 +1,16 @@
+
+
+class Schedule {
+	
+	/**
+	 *Schedule class is used to store events (ScheduleElement) by timing.
+	 *It can also detect conflicts.
+	 *
+	 *Schedule implements the following methods:
+	 *-
+	 *-
+	 */
+	
+	
+	
+}
---
+++ Dana and Mahmoud/ScheduleElement.java
@@ -0,0 +1,14 @@
+
+public class ScheduleElement {
+	
+	/**
+	 * ScheduleElement are used to make schedules.
+	 * a ScheduleElement has:
+	 * -A date
+	 * -A start time
+	 * -An end time
+	 * -A title
+	 * -A location
+	 */
+
+}
