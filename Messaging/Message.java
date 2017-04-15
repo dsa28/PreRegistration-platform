2b76df812423e29210ad195a9acd2774bf6e6b95
mahmoudsafar <mahmoudsafar@outlook.com>
2017-04-15 17:02:22 +0300

    Create Message.java

---
+++ Messaging/Message.java
@@ -0,0 +1,43 @@
+/**
+ * Created by mahmoudsafar on 4/15/17.
+ */
+public class Message {
+
+
+    //private id;
+    //private String timestamp;
+    private String content;
+    private int senderID;
+    private int receiverID;
+    private Boolean readStatus = false;
+
+    protected Message( int senderID, int receiverID, String content)
+    {
+        this.receiverID = receiverID;
+        this.senderID = senderID;
+        this.content = content;
+    }
+
+
+    int getReceiverID ()
+    {
+        return  receiverID;
+    }
+
+    int getSenderID ()
+    {
+        return receiverID;
+    }
+
+    String getContent()
+    {
+        return content;
+    }
+
+    void setReadStatus()
+    {
+        readStatus = true;
+    }
+
+
+}
