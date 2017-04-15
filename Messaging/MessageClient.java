0a955d95342a2d1e499b9636142a864fa1fd3c7c
mahmoudsafar <mahmoudsafar@outlook.com>
2017-04-15 17:02:50 +0300

    Create MessageClient.java

---
+++ Messaging/MessageClient.java
@@ -0,0 +1,46 @@
+import java.util.Vector;
+
+/**
+ * Created by mahmoudsafar on 4/15/17.
+ */
+public abstract class MessageClient {
+
+
+
+
+    void saveMessageToDB( Message M )
+    {
+     //Save message content and info into the database
+
+    }
+
+    Vector<Message> getInbox( int receiverID )
+    {
+
+        //Gets all messages of the user of ID == receiverID
+
+    }
+
+
+    String readMessageFromInbox (Message M)
+    {
+        String content = M.getContent();
+        M.setReadStatus();
+        updateMessageInDB(M);
+        return content;
+    }
+
+    void updateMessageInDB(Message M)
+    {
+        //Updates message reading status
+        //Can't this be done via another function?
+
+    }
+
+
+    void deleteMessageInDB (Message M)
+    {
+        //Deletes message in DB
+    }
+
+}
