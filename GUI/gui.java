dfa7dbaf06ad1423840f9763f862a3676ac91a4e
mahmoudsafar <mahmoudsafar@outlook.com>
2017-04-17 20:08:06 +0300

    Create gui.java

---
+++ GUI/gui.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by mahmoudsafar on 4/17/17.
+ */
+public class gui {
+    private JTextField usernameInput;
+    private JPanel form;
+    private JPasswordField passwordInput;
+    private JButton loginButton;
+    private JLabel errorLabel;
+    private JTabbedPane navigation;
+    private JButton refreshButton;
+    private JButton inboxButton;
+
+    //Navigation Tabs Indices --Not sure if needed but it does help...
+    private int loginTab = 0;
+    private int dashboardTab = 1;
+
+
+    // private UserSystem userSystem;
+
+    //Below should include all functions to be defined and triggered upon any input being done on the form
+
+
+    //Login Button Pressed
+    public gui() {
+        loginButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                //Submit input to user system
+                String username = usernameInput.getText();
+                char[] password = passwordInput.getPassword();
+                //boolean check = userSystem.checkCredentials(username, password);
+                /*
+                if (check == true)
+                {
+                    errorLabel.setText("");
+                    //Goes to next tab (dashboard of index 1)
+                    navigation.setSelectedIndex(1);
+                }
+
+                else
+                {
+                errorLabel.setText("Please enter correct credentials");
+                }
+                 */
+
+
+
+            }
+        });
+    }
+
+
+}
