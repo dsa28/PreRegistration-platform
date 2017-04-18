85f0de6bc998fd4fc043f8dd56e3120b20aee3c5
masjher <abe09@mail.aub.edu>
2017-04-18 19:50:36 +0300

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+/**
+ * Created by user on 16-Apr-17.
+ */
+public class Teacher {
+    ArrayList<String> courses;
+    data_storage d;
+    int dborder;
+    boolean isconflict=false;//to be edited
+    Teacher(ArrayList<String> course, data_storage dd, int order)
+    {
+        courses=course;
+        d=dd;
+        dborder=order;
+    }
+    public void addcourse(String c)
+    {
+        if(!isconflict) {
+            d.alluserinfo.get(dborder).add(c);
+            courses.add(c);
+        }
+    }
+    public void removecourse(String c)
+    {
+        for(int i=0;i<courses.size();i++)
+            if(courses.get(i).equals(c))
+                courses.remove(c);
+    }
+}
